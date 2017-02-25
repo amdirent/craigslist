06fda20de3ad8619c9d1d5739dd7a4b3d5552c80
Chris Hall <chall8908@gmail.com>
2017-02-24 18:25:03 -0600

    Remove reply information attempt
    It always gets blocked quickly, so it won't be particularly useful

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,12 +3,8 @@ require 'uri'
 require_relative 'utils'
 
 class Scraper
-  @@_skip_reply_data = false
-
   class << self
     def scrape(url, referer=nil)
-      base_host = URI.parse(url).host
-
       page = begin
                Nokogiri::HTML(Utils.get_page(url, { 'Referer' => referer }))
              rescue
@@ -20,36 +16,7 @@ class Scraper
 
       post_body = page.css('#postingbody').first.text.strip
 
-      unless @@_skip_reply_data
-        reply_link = page.css('#replylink').first
-
-        reply_uri = begin
-                      uri = URI.parse(reply_link['href'])
-                      uri.host ||= base_host
-                      uri.scheme ||= 'https'
-
-                      uri.to_s
-                    rescue
-                      p "No reply info found for #{url}"
-                      return [post_body, nil, nil]
-                    end
-
-        reply_data = Nokogiri::HTML(Utils.get_page(reply_uri, { 'Referer' => url }))
-
-        unless reply_data.css('.captcha').first.nil?
-          p "Getting captcha'd, skipping reply data from now on."
-          @@_skip_reply_data = true
-        end
-
-        reply_email = reply_data.css('.reply-email-address a').first.text rescue nil
-        reply_phone = begin
-                        reply_data.css('.reply-tel-link').first['href'].gsub(/\D/, '')
-                      rescue
-                        reply_data.css('.reply-tel-number').first.text.gsub(/\D/, '') rescue nil
-                      end
-      end
-
-      [post_body, reply_email, reply_phone]
+      [post_body, nil, nil]
     rescue => e
       p "#{e.class}: #{e.message}"
       [nil, nil, nil]
